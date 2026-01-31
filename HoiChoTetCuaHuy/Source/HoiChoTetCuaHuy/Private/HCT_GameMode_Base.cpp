d3dd05ca93e1446f4eb73814c74255e3ed8b40fb
HuyNguyen164 <nghoanganhhuy164@gmail.com>
2026-01-31 19:08:10 +0700

    Set default PlayerController, Pawn & HUD trong C++

--- HoiChoTetCuaHuy/Source/HoiChoTetCuaHuy/Private/HCT_GameMode_Base.cpp
+++ HoiChoTetCuaHuy/Source/HoiChoTetCuaHuy/Private/HCT_GameMode_Base.cpp
@@ -3,11 +3,15 @@
 
 #include "HoiChoTetCuaHuy/Public/HCT_GameMode_Base.h"
 
+#include "HCT_HUD_Base.h"
+#include "HCT_PlayerController_Base.h"
 #include "HoiChoTetCuaHuy/HoiChoTetCuaHuy.h"
 
 AHCT_GameMode_Base::AHCT_GameMode_Base()
 {
-	
+	PlayerControllerClass = AHCT_PlayerController_Base::StaticClass();
+	DefaultPawnClass = AHCT_PlayerController_Base::StaticClass();
+	HUDClass = AHCT_HUD_Base::StaticClass();
 }
 
 void AHCT_GameMode_Base::BeginPlay()
